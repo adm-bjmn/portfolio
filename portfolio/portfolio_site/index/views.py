db57af013e1959605a6878d275bb9ef6b769c303
adm.bjmn <adm.bjmn@gmail.com>
2023-01-31 21:05:27 +0000

    contact form and home page

--- portfolio/portfolio_site/index/views.py
+++ portfolio/portfolio_site/index/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index/home.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST['Name']
+        email = request.POST['Email']
+        message = request.POST['Message']
+        return render(request, 'contact/contact.html', {'email': email})
+    else:
+        return render(request, 'contact/contact.html', {})
